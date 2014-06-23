b893d0c14cf8ad5c77a05a39f40b2d05e3ee948f
Ian Fleeton <ianfleeton@gmail.com>
2014-06-23 19:18:00 +0100

    Basic response for request to copy

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(bodyParser.json());
+
 var port = 3002;
+
+app.post('/', function(req, res) {
+  console.log("Received request to copy " + req.body.source);
+  res.send(200);
+});
 
 app.listen(port);
 console.log("copy-to-s3 listening on port " + port + "...");
