541c037ae7efbfb90620f7035c5c8ae903c777ec
Ian Fleeton <ianfleeton@gmail.com>
2014-07-09 16:14:53 +0100

    Fetch URL to copy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var url = require('url');
+var http = require('http');
 
 var app = express();
 
@@ -9,7 +11,20 @@ var port = 3002;
 
 app.post('/', function(req, res) {
   console.log("Received request to copy " + req.body.source);
-  res.send(200);
+
+  var parsedUrl = url.parse(req.body.source);
+  console.log(parsedUrl);
+  var sourceReq = http.get(parsedUrl, function(sourceRes) {
+    console.log('STATUS: ' + sourceRes.statusCode);
+    sourceRes.on('data', function(chunk) {
+      console.log('BODY: ' + chunk);
+    });
+    res.send(sourceRes.statusCode);
+  });
+  sourceReq.on('error', function(err) {
+    console.log(err);
+    res.send(500);
+  });
 });
 
 app.listen(port);
