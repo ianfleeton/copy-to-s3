bdb1c8824d041c508b941b05019d657e4e7333b7
Ian Fleeton <ianfleeton@gmail.com>
2014-07-09 17:27:17 +0100

    Upload to S3

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var url = require('url');
 var http = require('http');
+var Uploader = require('s3-streaming-upload').Uploader;
 
 var app = express();
 
 app.use(bodyParser.json());
 
-var port = 3002;
+var port = process.env.COPY_TO_S3_PORT;
 
 app.post('/', function(req, res) {
   console.log("Received request to copy " + req.body.source);
@@ -15,11 +16,37 @@ app.post('/', function(req, res) {
   var parsedUrl = url.parse(req.body.source);
   console.log(parsedUrl);
   var sourceReq = http.get(parsedUrl, function(sourceRes) {
-    console.log('STATUS: ' + sourceRes.statusCode);
-    sourceRes.on('data', function(chunk) {
-      console.log('BODY: ' + chunk);
-    });
-    res.send(sourceRes.statusCode);
+    console.log('Fetch STATUS: ' + sourceRes.statusCode);
+
+    if(sourceRes.statusCode == 200) {
+      var objectName = parsedUrl.hostname + parsedUrl.pathname;
+
+      var upload = new Uploader({
+        accessKey:  process.env.COPY_TO_S3_AWS_API_KEY,
+        secretKey:  process.env.COPY_TO_S3_AWS_SECRET,
+        bucket:     process.env.COPY_TO_S3_AWS_BUCKET,
+        objectName: objectName,
+        stream:     sourceRes,
+        objectParams: {
+          ACL: 'public-read'
+        }
+      });
+
+      upload.on('completed', function (err, s3Res) {
+        console.log('upload completed');
+        res.send(200);
+      });
+
+      upload.on('failed', function (err) {
+        console.log('upload failed with error', err);
+        upload.finishUploads();
+        if(!res.headersSent) {
+          res.send(500);
+        }
+      });
+    } else {
+      res.send(sourceRes.statusCode);      
+    }
   });
   sourceReq.on('error', function(err) {
     console.log(err);
